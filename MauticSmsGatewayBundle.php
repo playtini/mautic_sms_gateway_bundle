67d9e29ecd5249ba373134f6e7a8aed0600963ed
Valery <walera3gp@gmail.com>
2023-08-02 18:14:22 +0300

    Mautic 5

--- MauticSmsGatewayBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MauticPlugin\MauticSmsGatewayBundle;
-
-use Mautic\IntegrationsBundle\Bundle\AbstractPluginBundle;
-
-class MauticSmsGatewayBundle extends AbstractPluginBundle
-{
-}
