47b1cb137f61cd6648a29a9567b9910fd4fee0f8
Valery <walera3gp@gmail.com>
2024-03-28 18:34:56 +0200

    Mautic 5, fix

--- Config/services.php
+++ Config/services.php
@@ -15,6 +15,6 @@ return function (ContainerConfigurator $configurator) {
     $excludes = [
     ];
 
-    $services->load('MauticPlugin\\MauticAdvancedTemplatesBundle\\', '../')
+    $services->load('MauticPlugin\\MauticSmsGatewayBundle\\', '../')
         ->exclude('../{'.implode(',', array_merge(MauticCoreExtension::DEFAULT_EXCLUDES, $excludes)).'}');
 };
